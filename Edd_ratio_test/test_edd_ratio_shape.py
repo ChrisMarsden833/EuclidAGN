fc56485d68451e965b50edb17a5551ec906ddb9d
RugiadaC <carrarorosamaria@gmail.com>
2020-08-07 17:29:23 -0400

    Completed macro to plot Eddington distribution shapes for Schechter function and Gaussian distribution.

--- Edd_ratio_test/test_edd_ratio_shape.py
+++ Edd_ratio_test/test_edd_ratio_shape.py
@@ -1,4 +1,4 @@
-%## 
+#%%
 """
 Test Eddington ratio distribution shapes
 """
@@ -8,21 +8,29 @@ import matplotlib.pyplot as plt
 # Edd ratio values range
 edd_bin = np.arange(-4, 0, 0.001)
 edd=10 ** edd_bin
+
+def schechter(edd,arg1,arg2):
+   prob = ((edd / (10. ** arg1)) ** arg2)
+   return prob* np.exp(-(edd / (10. ** arg1)))
+
+def gaussian(edd,arg1,arg2):
+   return np.exp(-(edd - arg2) ** 2. / (2.*arg1 ** 2.))
 #%%
 # Shankar + Schechter z=1
 arg1=-0.8
 arg2= 1.6
 
-prob = ((edd / (10. ** arg1)) ** arg2)
-prob *= np.exp(-(edd / (10. ** arg1)))
-
-plt.plot(edd,prob)
+plt.plot(edd,schechter(edd,arg1,arg2));
 #%%
 # shankar + gaussian
 arg1= 0.2
 arg2= 0.25
 
-prob=np.exp(-(edd - arg2) ** 2. / (2.*arg1 ** 2.))
+plt.plot(edd,gaussian(edd,arg1,arg2));
+#%%
+# shankar + gaussian
+arg1= 0.2
+arg2= 0.6
 
-plt.plot(edd,prob);
+plt.plot(edd,gaussian(edd,arg1,arg2));
 # %%
